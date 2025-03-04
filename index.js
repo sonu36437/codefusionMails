cfc8ee6fd68b841076fc4d4fd78ee66bd674f5e0
sonu kumar <sonu36437@gmail.com>
2025-03-04 23:20:49 +0530

    trying to solve bugs

--- index.js
+++ index.js
@@ -99,8 +99,9 @@ app.get('/get-mails/:date', async (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
+ connectToDatabase();
 app.listen(PORT, async() => {
- await connectToDatabase();
+
 
   
   console.log(`Server is running on port ${PORT}`);
